33f84c9dca3a3988d46b514806e57caa4a494d52
Kim Min Sung <56097431+minsung8@users.noreply.github.com>
2020-09-06 19:03:05 +0900

    Add files via upload

--- solution6.py
+++ solution6.py
@@ -1,57 +1,45 @@
 # 프로그래머스 : https://programmers.co.kr/learn/courses/30/lessons/60060
 
-class Node:
-    def __init__(self, w = ""):
-        self.word = w
-        self.cList = {}         ## 자식노드 저장
-        self.length = len(w)        ## 마지막 노드에 그 단어 길이 저장
+import os
+import sys
 
-class Tree:
+from collections import defaultdict
+
+class Trie:
+
     def __init__(self):
-        self.root = Node("")
-        self.keyList = {}           ## 각 단어와 길이 dictionary로 저장
+        self.root = {} # chr|visited
 
-    def create(self,  words, reverse=False):
-        for word in words:
-            if word not in self.keyList:
-                self.keyList[word] = len(word)
-                cur_node = self.root
-                for w in word:
-                    if w not in cur_node.cList:
-                        cur_node.cList[w] = Node(w)
-                    cur_node = cur_node.cList[w]
-                cur_node.length = len(word)
+    def insert(self, s):
+        cur = self.root
+        while s:
+            if s[0] not in cur: cur[s[0]] = [ {} , 0 ]
+            cur[s[0]][1] += 1
+            cur = cur[s[0]][0]
+            s = s[1:]
 
-    def search(self, word):
-        num = 0
-        cur_node = self.root
-        temp = []
-        for w in word:
-            if w == "?":
-                for node in cur_node.cList.values():
-                    temp.append(node)
-                while temp:
-                    temp_node = temp.pop()
-                    if temp_node.length == len(word):
-                        num += 1
-                    else:
-                        for node in temp_node.cList.values():
-                            temp.append(node)
-                return num
-            elif w in cur_node.cList:
-                cur_node = cur_node.cList[w]
+
+    def find(self, s)->int:
+        cur = self.root; pre_v = 0
+        while s:
+            if s[0] == '?': return pre_v
             else:
-                return num
+                if s[0] not in cur: return 0
+                pre_v = cur[s[0]][1]; cur = cur[s[0]][0]
+            s = s[1:]
+
+        return pre_v
 
 def solution(words, queries):
-    dic = {}
-    answer = []
-    tree = Tree()
-    tree.create(words, False)
+    prefix_dict = defaultdict(Trie)
+    result = []
+
+    for word in words:
+        prefix_dict[len(word)].insert(word)
 
     for q in queries:
-        if q not in dic:
-            answer.append(tree.search(q))
-        else:
-            answer.append(dic[q])
-    return answer
+        result.append(prefix_dict[len(q)].find(q))
+
+    return result
+
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
