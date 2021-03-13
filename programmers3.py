76c6d409241fc074ecb98881629e6313d86e88dc
Kim Min Sung <alstjddl8@naver.com>
2021-03-13 16:24:58 +0900

    Add files via upload

--- programmers3.py
+++ programmers3.py
@@ -2,52 +2,53 @@
 
 def solution(sticker):
 
+    if len(sticker) < 4:
+        return max(sticker)
+
     answer_list = []
 
+    # 첫번째 시작
+    first_sticker = sticker[:-1]
     idx = 0
-
-    temp = 0
-
-    last_value = sticker.pop(-1)
-
-    while idx < len(sticker):
-
-        temp += sticker[idx]
-
-        if idx + 4 < len(sticker) and (sticker[idx + 3] > sticker[idx + 2] + sticker[idx + 4]):
+    temp_answer = 0
+    while idx < len(first_sticker):
+        temp_answer += first_sticker[idx]
+        if idx + 3 < len(first_sticker) and first_sticker[idx + 3] > first_sticker[idx + 2]:
             idx += 3
-        
-        elif idx + 3 < len(sticker) and sticker[idx + 3] > sticker[idx + 2]:
-            idx += 3
-        
         else:
             idx += 2
-    
-    answer_list.append(temp)
-    sticker += [last_value]
+    answer_list.append(temp_answer)
 
-    idx = 1
+    # 두번째 시작
+    sec_sticker = sticker[1:]
+    idx = 0
+    temp_answer = 0
+    while idx < len(sec_sticker):
+        temp_answer += sec_sticker[idx]
 
-    temp = 0
-
-    while idx < len(sticker):
-
-        temp += sticker[idx]
-
-        if idx + 4 < len(sticker) and (sticker[idx + 3] > sticker[idx + 2] + sticker[idx + 4]):
+        if idx + 3 < len(sec_sticker) and sec_sticker[idx + 3] > sec_sticker[idx + 2]:
             idx += 3
-        
-        elif idx + 3 < len(sticker) and sticker[idx + 3] > sticker[idx + 2]:
-            idx += 3
-        
         else:
             idx += 2
-    
-    answer_list.append(temp)
-        
+    answer_list.append(temp_answer)
+
+    # 세번째 시작
+    third_sticker = sticker[2:]
+    idx = 0
+    temp_answer = 0
+    while idx < len(third_sticker):
+        temp_answer += third_sticker[idx]
+
+        if idx + 3 < len(third_sticker) and third_sticker[idx + 3] > third_sticker[idx + 2]:
+            idx += 3
+        else:
+            idx += 2
+    answer_list.append(temp_answer)
+
+    # 그 중 가장 큰 값
     return max(answer_list)
 
-## temp = 25 idx = 3 / temp = 34 idx = 5
+print(solution([14, 6, 5, 7, 3, 9, 2, 10, 8, 4, 22, 4, 4, 8, 100, 1]))
+#print(solution([1, 3, 2, 5, 4]))
+#print(solution([1, 1, 1, 1, 1]))
 
-print(solution([14, 6, 5, 11, 3, 9, 2, 10]))
-print(solution([1, 3, 2, 5, 4]))
