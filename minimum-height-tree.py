9a3df5d921b65348eccd51a3421e5015abaf2b0b
Kim Min Sung <alstjddl8@naver.com>
2021-05-20 21:13:33 +0900

    Add files via upload

--- minimum-height-tree.py
+++ minimum-height-tree.py
@@ -1,37 +1,47 @@
-import collections
+from collections import defaultdict
 
 def solution(n, edges):
+    
+    point = []
 
-    dic = collections.defaultdict(list)
+    for i in range(n):
+        point.append(i + 1)
 
-    for i in range(len(edges)):
+    while True:
 
-        x = edges[i][0]
-        y = edges[i][1]
-        dic[x].append(y)
-        dic[y].append(x)
+        dic = defaultdict(list)
 
-    while dic:
-        answer = set()
-        temp = dic.keys()
-        print(temp)
-        for key in temp:
+        for i in range(len(edges)):
+            x = edges[i][0]
+            y = edges[i][1]
 
-            if len(dic[key]) == 1:
-                temp_value = dic[key].pop()
-                answer.add(temp_value)
-                dic[temp_value].pop( dic[temp_value].index(key) )
-        print(answer)
-        flag = True
-        answer = list(answer)
-        for i in range(len(answer)):
-            
-            if len(dic[ answer[i] ]) == 0:
-                flag = False
+            dic[x].append(y)
+            dic[y].append(x)
+
+        temp = []
         
-        if flag is False:
-            return answer
+        for key in dic.keys():
+            
+            if len( dic[key] ) == 1:
+                temp.append([key, dic[key][0]])
+                point.pop( point.index(key) )
+
+        while temp:
+            start = temp.pop(0)
+
+            if start in edges:
+                edges.pop( edges.index(start) )
+            else:
+                start.reverse()
+                edges.pop( edges.index( start ) )
+        
+        if len(edges) < 2:
+            return point
+    
+    return 
+
 
         
 #print(solution(4, [[1, 0], [1, 2], [1, 3]]))
-print(solution(6, [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4]]))
+#print(solution(6, [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4]]))
+print( solution(10, [[1, 3], [2, 3], [3, 4], [3, 5], [4, 6], [6, 10], [5, 7], [5, 8], [8, 9]]) )
