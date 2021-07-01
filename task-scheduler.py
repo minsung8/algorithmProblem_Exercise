23c04a2968e8de4f22bb4a751eb6616d4ed6ca8c
Kim Min Sung <alstjddl8@naver.com>
2021-07-02 08:38:05 +0900

    Add files via upload

--- task-scheduler.py
+++ task-scheduler.py
@@ -12,23 +12,33 @@ def solution(tasks, n):
         
         elif new_tasks[-1][0] != task:
             new_tasks.append([task, 1])
-    
+
     complete_list = []
     i = 0
-    while True:
-
-
-        if complete_list[-1] != new_tasks[i][0] and complete_list[-2] != new_tasks[i][0]:
+    while new_tasks:
+        print(complete_list, new_tasks)
+        print(complete_list[-n:], new_tasks[i][0])
+        if len(complete_list) < n:
             complete_list.append(new_tasks[i][0])
             new_tasks[i][1] -= 1
+        elif new_tasks[i][0] not in complete_list[-n:]:
+            complete_list.append(new_tasks[i][0])
+            new_tasks[i][1] -= 1
+        else:
+            complete_list.append('idle')
+            i -= 1
+
+        if new_tasks[i][1] == 0:
+            new_tasks.pop(i)
+        else:
             i += 1
 
-        if i == len(new_tasks) - 1:
+        if i == len(new_tasks):
             i = 0
         
-        return 
+        if len(complete_list) != 0 and (len(complete_list) + 1)% n == 0 and i != 0:
+            i = 0
 
+    return complete_list
 
-    return new_tasks
-
-print(solution(["A", "A", "A", "B", "B", "B"], 2))
+print(solution(["A", "A", "A", "B", "C", "D"], 2))
