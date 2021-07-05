9fae27c68276a461d7fb94811923de21ecc27f47
Kim Min Sung <alstjddl8@naver.com>
2021-07-05 16:59:33 +0900

    Add files via upload

--- task-scheduler.py
+++ task-scheduler.py
@@ -1,44 +1,41 @@
 def solution(tasks, n):
+    
     new_tasks = []
-    tasks.sort()
 
-    for task in tasks:
+    for task in set(tasks):
+        new_tasks.append([task, tasks.count(task)])
+    new_tasks = sorted(new_tasks, key=lambda x: x[1], reverse=True)
 
-        if len(new_tasks) == 0:
-            new_tasks.append([task, 1])
-        
-        elif new_tasks[-1][0] == task:
-            new_tasks[-1][1] += 1
-        
-        elif new_tasks[-1][0] != task:
-            new_tasks.append([task, 1])
-
-    complete_list = []
+    answer = []
     i = 0
-    while new_tasks:
-        print(complete_list, new_tasks)
-        print(complete_list[-n:], new_tasks[i][0])
-        if len(complete_list) < n:
-            complete_list.append(new_tasks[i][0])
-            new_tasks[i][1] -= 1
-        elif new_tasks[i][0] not in complete_list[-n:]:
-            complete_list.append(new_tasks[i][0])
+
+    while True:
+        if new_tasks[i][0] not in answer[-n:]:
+            answer.append(new_tasks[i][0])
             new_tasks[i][1] -= 1
         else:
-            complete_list.append('idle')
-            i -= 1
+            answer.append('idle')
 
         if new_tasks[i][1] == 0:
             new_tasks.pop(i)
-        else:
-            i += 1
+            i -= 1
+        i += 1
+
+        if not new_tasks:
+            break
+
+        if len(answer) != 0 and len(answer) % n == 0 and answer[-1] != new_tasks[0][0]:
+            if new_tasks[0][0] in answer[-2:]:
+                answer.append('idle')
+            i = 0
 
         if i == len(new_tasks):
             i = 0
-        
-        if len(complete_list) != 0 and (len(complete_list) + 1)% n == 0 and i != 0:
-            i = 0
 
-    return complete_list
+    return len(answer)
 
 print(solution(["A", "A", "A", "B", "C", "D"], 2))
+print(solution(["A", "A", "A", "B", "B", "B"], 2))
+print(solution(["A", "A", "A"], 2))
+
+
