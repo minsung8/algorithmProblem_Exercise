de18ebf7d4474ecfa5d5093bcc75ede91857e7a6
Kim Min Sung <alstjddl8@naver.com>
2021-07-12 08:24:07 +0900

    Add files via upload

--- dif-ways-to-add-parentheses.py
+++ dif-ways-to-add-parentheses.py
@@ -1,35 +1,38 @@
 answer = []
 
 def solution(s):
-    
-    if len(s) == 3:
-        answer.append([cal(s)])
-        return
-
-    for i in range(len(s)):
+    print(s)
         
+    if cnt(s) == 1:
+        return cal(s)
+
+    if s.isnumeric():
+        return s
+
+    for i in range(1, len(s)):
         if s[i] in ['*', '-', '+']:
             temp_left = s[:i]
             temp_right = s[i + 1:]
 
-            if len(temp_left) == 1:
-                answer.append([temp_left])
-            if len(temp_right) == 1:
-                answer.append([temp_right])
-
-            solution(temp_left)
-            solution(temp_right)
+            answer.append(int( solution( solution(temp_left) + s[i] + solution(temp_right) ) ))
 
     return answer
 
 def cal(s):
     
     if s[1] == '+':
-        return int(s[0]) + int(s[2])
+        return str( int(s[0]) + int(s[2]) )
     elif s[1] == '-':
-        return int(s[0]) - int(s[2])
+        return str( int(s[0]) - int(s[2]) )
     elif s[1] == '*':
-        return int(s[0]) * int(s[2])
+        return str( int(s[0]) * int(s[2]) )
+
+def cnt(s):
+    cnt = 0
+    for i in range(1, len(s)):
+        if s[i] in ['*', '-', '+']:
+            cnt += 1
+    return cnt
 
 #print(solution("2-1-1"))
 print(solution("2*3-4*5"))
