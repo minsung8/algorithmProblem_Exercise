983f458e2779787dc8f8479f240fe19ea670dec3
Kim Min Sung <alstjddl8@naver.com>
2021-07-13 08:28:52 +0900

    Add files via upload

--- dif-ways-to-add-parentheses.py
+++ dif-ways-to-add-parentheses.py
@@ -1,39 +1,35 @@
 answer = []
 
 def solution(s):
-    print(s)
+    
+    for i in range(len(s)):
         
-    if cnt(s) == 1:
-        return cal(s)
-
-    if s.isnumeric():
-        return s
-
-    for i in range(1, len(s)):
         if s[i] in ['*', '-', '+']:
-            temp_left = s[:i]
-            temp_right = s[i + 1:]
-
-            answer.append(int( solution( solution(temp_left) + s[i] + solution(temp_right) ) ))
+            left = s[:i]
+            right = s[i + 1:]
+        
+            left_list = dfs(left)
+            right_list = dfs(right)
+            print(left_list, right_list)
 
     return answer
 
-def cal(s):
-    
-    if s[1] == '+':
-        return str( int(s[0]) + int(s[2]) )
-    elif s[1] == '-':
-        return str( int(s[0]) - int(s[2]) )
-    elif s[1] == '*':
-        return str( int(s[0]) * int(s[2]) )
 
-def cnt(s):
-    cnt = 0
-    for i in range(1, len(s)):
-        if s[i] in ['*', '-', '+']:
-            cnt += 1
-    return cnt
+def dfs(s):
 
-#print(solution("2-1-1"))
-print(solution("2*3-4*5"))
+    answer = []
+
+    if s.isnumeric():
+        return [s]
+
+    for i in range(len(s)):
+        if s[i] in '*-+':
+            answer.append(eval( str(eval(s[i:])) + s[i] + str(eval(s[i+1:])) ))
+
+    return answer
+
+
+print(solution('3-4'))
+print(solution("2-1-1"))
+#print(solution("2*3-4*5"))
 #print(solution("2*3-4*5+3*12"))
