ffc1a309e33efe938ca91ec3101c20647f2c099f
Kim Min Sung <alstjddl8@naver.com>
2021-07-15 07:42:15 +0900

    Add files via upload

--- dif-ways-to-add-parentheses.py
+++ dif-ways-to-add-parentheses.py
@@ -1,30 +1,32 @@
-result = []
 
 def solution(s):
 
-    dfs(s)
+    answer = dfs(s)
 
-    return result
+    for i in range(len(answer)):
+        answer[i] = int(answer[i])
+    return answer
 
 def dfs(s):
-    if s.isnumeric():
-        return s
 
-    global result
+    if s.isnumeric():
+        return [s]
+    answer = []
 
     for i in range(len(s)):
+        left_list = []
+        right_list = []
         if s[i] in '*-+':
-            print(s[:i], s[i], s[i+1:], result)
-            print(eval(dfs(s[:i]) + s[i] + dfs(s[i+1:])) )
-            result.append( eval(dfs(s[:i]) + s[i] + dfs(s[i+1:])) )
-            print(result)
+            left_list = dfs(s[:i])
+            right_list = dfs(s[i+1:])
+            for x in range(len(left_list)):
+                for y in range(len(right_list)):
+                    answer.append( str(eval(left_list[x] + s[i] + right_list[y])))
 
-    return
+    return answer
 
 
-
-
-#print(solution('3-4'))
-#print(solution("2-1-1"))
+print(solution('3-4'))
+print(solution("2-1-1"))
 print(solution("2*3-4*5"))
-#print(solution("2*3-4*5+3*12"))
+print(solution("2*3-4*5+3*12"))
