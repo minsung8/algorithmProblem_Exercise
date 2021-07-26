fdcb31b4f58f124180af9b10c8184a891124fc11
Kim Min Sung <alstjddl8@naver.com>
2021-07-27 08:25:29 +0900

    Add files via upload

--- programmers006.py
+++ programmers006.py
@@ -2,7 +2,7 @@
 
 def solution(n, k, cmd):
 
-    answer = list('OOOOOOOO')
+    answer = list('O' * n)
     delete_list = []
     for i in range(len(cmd)):
         if cmd[i][0] == 'D':
@@ -26,18 +26,23 @@ def solution(n, k, cmd):
         elif cmd[i][0] == 'C':
             answer[k] = 'X'
             delete_list.append(k)
-            k += 1
-            if k > len(answer) - 1 or 'O' not in answer[k:]:
-                for j in range(len(answer)):
+            
+            if k == len(answer) - 1:
+                for j in range(k - 1, -1, -1):
                     if answer[j] == 'O':
                         k = j
+                        break
+            else:
+                for j in range(k + 1, len(answer)):
+                    if answer[j] == 'O':
+                        k = j
+                        break
 
         elif cmd[i][0] == 'Z':
             temp_idx = delete_list.pop(-1)
             answer[temp_idx] = 'O'
-
+        
     return "".join(answer)
 
-
-print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]))
+#print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]))
 print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]))
