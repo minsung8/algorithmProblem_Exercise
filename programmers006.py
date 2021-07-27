14aa34ab2a7d6bffd6a645c6c78e16977b7326d8
Kim Min Sung <alstjddl8@naver.com>
2021-07-28 08:36:57 +0900

    Add files via upload

--- programmers006.py
+++ programmers006.py
@@ -1,48 +1,52 @@
 # https://programmers.co.kr/learn/courses/30/lessons/81303
+from collections import defaultdict
 
 def solution(n, k, cmd):
 
     answer = list('O' * n)
+    dic = {}
     delete_list = []
+    dic = defaultdict(list)
+    dic[0] = [None, 1]
+    dic[n - 1] = [n - 2, None]
+    for i in range(1, n-1):
+        dic[i] = [i - 1, i + 1]
+    
+    return dic
     for i in range(len(cmd)):
         if cmd[i][0] == 'D':
             temp_cnt = int(cmd[i].split(' ')[1])
-            for j in range(k + 1, len(answer)):
-                if answer[j] == 'O':
-                    temp_cnt -= 1
-                if temp_cnt == 0:
-                    k = j
-                    break
+            k = current_list[current_list.index(k) + temp_cnt]
 
         elif cmd[i][0] == 'U':
             temp_cnt = int(cmd[i].split(' ')[1])
-            for j in range(k - 1, -1, -1):
-                if answer[j] == 'O':
-                    temp_cnt -= 1
-                if temp_cnt == 0:
-                    k = j
-                    break
+            k = current_list[current_list.index(k) - temp_cnt]
 
         elif cmd[i][0] == 'C':
-            answer[k] = 'X'
             delete_list.append(k)
-            
+
             if k == len(answer) - 1:
-                for j in range(k - 1, -1, -1):
-                    if answer[j] == 'O':
-                        k = j
-                        break
+                temp = k
+                k = current_list[current_list.index(k) - 1]
+                current_list.pop(current_list.index(temp))
+
             else:
-                for j in range(k + 1, len(answer)):
-                    if answer[j] == 'O':
-                        k = j
-                        break
+                temp = k
+                k = current_list[current_list.index(k) + 1]
+                current_list.pop(current_list.index(temp))
 
         elif cmd[i][0] == 'Z':
             temp_idx = delete_list.pop(-1)
-            answer[temp_idx] = 'O'
-        
-    return "".join(answer)
+            current_list.append(temp_idx)
 
-#print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]))
+    result = ''
+    for i in range(len(answer)):
+        if i in current_list:
+            result += 'O'
+        else:
+            result += 'X'
+
+    return result
+
+print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]))
 print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]))
