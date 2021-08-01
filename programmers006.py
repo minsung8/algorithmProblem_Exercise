5784f2768626646e5759054e64345185aff81413
Kim Min Sung <alstjddl8@naver.com>
2021-08-02 08:14:05 +0900

    Add files via upload

--- programmers006.py
+++ programmers006.py
@@ -1,27 +1,55 @@
 # https://programmers.co.kr/learn/courses/30/lessons/81303
 
 def solution(n, k, cmd):
-    answer = 'O' * n
-    linked_list = { i : [i - 1, i + 1] for i in range(1, n - 1)}
-    linked_list[n - 1] = {n - 2, None}
-    linked_list[0] = {None, 1}
+
+    answer = ['O'] * n
+    linked_list = { i : [i - 1, i + 1] for i in range(1, n + 1)}
+    linked_list[0] = [0, 1]
+    delete_list = []
 
     for i in range(len(cmd)):
-
         if cmd[i][0] == "D":
-            k += int(cmd[i].split(' ')[1])
+            for j in range(int(cmd[i][2:])):
+                k = linked_list[k][1]
 
         elif cmd[i][0] == "U":
-            k -= int(cmd[i].split(' ')[1])
+            for j in range(int(cmd[i][2:])):
+                k = linked_list[k][0]
 
         elif cmd[i][0] == "C":
+            print (linked_list, k)
             up, down = linked_list[k]
-            linked_list[up][1] = down
-            linked_list[down][0] = up
-            return linked_list
+            delete_list.append(k)
+
+            if down == n + 1:
+                k = linked_list[k][0]
+            else:
+                k = linked_list[k][1]
+
+            if up == 0:
+                linked_list[down][0] = up
+            elif down == n + 1:
+                linked_list[up][1] = down
+            else:
+                linked_list[down][0] = up
+                linked_list[up][1] = down
 
 
-    return linked_list
+        elif cmd[i][0] == "Z":
+            temp = delete_list.pop(-1)
+            up, down = linked_list[temp]
+            if up == 0:
+                linked_list[down][0] = temp
+            elif down == n + 1:
+                linked_list[up][1] = temp
+            else:
+                linked_list[down][0] = temp
+                linked_list[up][1] = temp
+            
+    for i in range(len(delete_list)):
+        answer[delete_list[i]] = 'X'
 
-print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]))
+    return "".join(answer)
+
+print(solution(8, 0, ["C"]))
 #print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]))
