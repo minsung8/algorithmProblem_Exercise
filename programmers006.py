fb45758d43db56781033a3e59e47f5e1ce193f14
Kim Min Sung <alstjddl8@naver.com>
2021-08-03 08:25:07 +0900

    Add files via upload

--- programmers006.py
+++ programmers006.py
@@ -2,9 +2,10 @@
 
 def solution(n, k, cmd):
 
-    answer = ['O'] * n
-    linked_list = { i : [i - 1, i + 1] for i in range(1, n + 1)}
-    linked_list[0] = [0, 1]
+    linked_list = { i : [i - 1, i + 1] for i in range(1, n - 1)}
+    linked_list[n - 1] = [n - 2, n]
+    linked_list[0] = [-1, 1]
+    answer = ["O" for i in range(n)]
     delete_list = []
 
     for i in range(len(cmd)):
@@ -17,39 +18,37 @@ def solution(n, k, cmd):
                 k = linked_list[k][0]
 
         elif cmd[i][0] == "C":
-            print (linked_list, k)
             up, down = linked_list[k]
-            delete_list.append(k)
+            delete_list.append([up, down, k])
+            answer[k] = "X"
 
-            if down == n + 1:
-                k = linked_list[k][0]
+            if down == n:
+                k = up
             else:
-                k = linked_list[k][1]
+                k = down
 
-            if up == 0:
+            if up == -1:
                 linked_list[down][0] = up
-            elif down == n + 1:
+            elif down == n:
                 linked_list[up][1] = down
             else:
                 linked_list[down][0] = up
                 linked_list[up][1] = down
-
 
         elif cmd[i][0] == "Z":
-            temp = delete_list.pop(-1)
-            up, down = linked_list[temp]
-            if up == 0:
-                linked_list[down][0] = temp
-            elif down == n + 1:
-                linked_list[up][1] = temp
+            up, down, now = delete_list.pop(-1)
+            answer[now] = "O"
+
+            if up == -1:
+                linked_list[down][0] = now
+            elif down == n:
+                linked_list[up][1] = now
             else:
-                linked_list[down][0] = temp
-                linked_list[up][1] = temp
-            
-    for i in range(len(delete_list)):
-        answer[delete_list[i]] = 'X'
+                linked_list[down][0] = now
+                linked_list[up][1] = now
 
     return "".join(answer)
 
 print(solution(8, 0, ["C"]))
-#print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]))
+
+print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]))
