8919d70f19c6dea51406dae68f95b66478c579a3
Kim Min Sung <alstjddl8@naver.com>
2021-08-08 13:44:06 +0900

    Add files via upload

--- programmers008.py
+++ programmers008.py
@@ -1,44 +1,49 @@
-temp_answer = []
-
 def solution(n, s, a, b, fares):
 
-    global temp_answer
-
+    cost_dic = {}
     answer = []
 
-    for i in range(n):
-        answer.append( [i+1, min(dfs(s, i + 1, 0, fares, []))] )
-        temp_answer = []
-    
-    result = []
+    for i in range(len(fares)):
+        if fares[i][0] not in cost_dic:
+            cost_dic[fares[i][0]] = [[fares[i][1]], [fares[i][2]]]
+        else:
+            cost_dic[fares[i][0]][0].append(fares[i][1])
+            cost_dic[fares[i][0]][1].append(fares[i][2])
 
-    for i in range(len(answer)):
-        pass
-    
+        if fares[i][1] not in cost_dic:
+            cost_dic[fares[i][1]] = [[fares[i][0]], [fares[i][2]]]
+        else:
+            cost_dic[fares[i][1]][0].append(fares[i][0])
+            cost_dic[fares[i][1]][1].append(fares[i][2])
+
+    for i in range(1, n + 1):
+        start_cost = min(dfs(s, i, cost_dic))
+        answer.append( start_cost + min(dfs(i, a, cost_dic)) + min(dfs(i, b, cost_dic)))
     return answer
 
-def dfs(start, end, cost, fares, visited):
 
-    global temp_answer
+def dfs(start, end, cost_dic):
+
     if start == end:
         return [0]
 
-    visited += [start]
+    answer = []
+    temp_list = [[start, 0]]
+    visited = []
+    while temp_list:
+        # 4
+        temp_start, temp_cost = temp_list.pop(0)
+        visited.append(temp_start)
+        #[1, 6, 2]
+        for i in range(len(cost_dic[temp_start][0])):
 
-    for i in range(len(fares)):
+            if cost_dic[temp_start][0][i] == end:
+                answer.append(temp_cost + cost_dic[temp_start][1][i])
+            else:
+                if cost_dic[temp_start][0][i] not in visited:
+                    temp_list.append([cost_dic[temp_start][0][i], temp_cost + cost_dic[temp_start][1][i]])
 
-        if (fares[i][0] == start and fares[i][1] == end) or (fares[i][1] == start and fares[i][0] == end):
-            temp_answer.append(cost + fares[i][2])
-        
-        elif fares[i][0] == start and fares[i][1] != end and fares[i][1] not in visited:
-            temp_visited = visited.copy()
-            dfs(fares[i][1], end, cost + fares[i][2], fares, temp_visited)
-         
-        elif fares[i][1] == start and fares[i][0] != end and fares[i][0] not in visited:
-            temp_visited = visited.copy()
-            dfs(fares[i][0], end, cost + fares[i][2], fares, temp_visited)
-
-    return temp_answer
+    return answer
 
 print(solution(6, 4, 6, 2, [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]))
 #print(dfs(4, 1, 0, [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]], []))
