0abcdfac08e5754c9dd50bac16c8f4b7de438047
Kim Min Sung <alstjddl8@naver.com>
2021-08-17 08:29:32 +0900

    Add files via upload

--- programmers009.py
+++ programmers009.py
@@ -6,9 +6,7 @@ def solution(play_time, adv_time, logs):
         temp_start = logs[i].split('-')[0]
         temp_end = cal(temp_start, adv_time, '+')
         temp_answer = ''
-        for j in range(len(logs)):
-            temp_start2 = logs[j].split('-')[0]
-            temp_end2 = logs[j].split('-')[1]
+
 
     answer = ''
 
@@ -21,7 +19,7 @@ def cal(t1, t2, s, multi=None):
         h2, m2, s2 = t2.split(':')
         total = ((int(h1) + int(h2)) * 3600) + ((int(m1) + int(m2)) * 60) + ((int(s1) + int(s2)))
 
-    elif s == '+':
+    elif s == '-':
             h1, m1, s1 = t1.split(':')
             h2, m2, s2 = t2.split(':')
             total1 = (int(h1) * 3600) + (int(m1) * 60) + (int(s1))
@@ -49,7 +47,6 @@ def cal(t1, t2, s, multi=None):
         answer += str(temp)    
 
     return answer 
-
 
 
 print(solution("02:03:55", "00:14:15", ["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"]))
