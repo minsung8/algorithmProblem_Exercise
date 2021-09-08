c535fc9bdabbee8de2d586224e4d3a26ff6cfc41
Kim Min Sung <alstjddl8@naver.com>
2021-09-09 08:14:35 +0900

    Add files via upload

--- programmers009.py
+++ programmers009.py
@@ -12,32 +12,17 @@ def solution(play_time, adv_time, logs):
     for i in range(1, len(play_list)):
         play_list[i] += play_list[i - 1]
 
+    logs = sorted(logs)
+
     answer = 0
     answer_score = play_list[minute(adv_time)] - play_list[0]
 
-    logs = sorted(logs)
-    for log in logs:
-        temp_start, temp_end = log.split('-')
+    for i in range(1, len(play_list)):
+        if i + minute(adv_time) <= minute(play_time) and \
+            answer_score < play_list[i + minute(adv_time)] - play_list[i]:
+            answer = i + 1
+            answer_score = play_list[i + minute(adv_time)] - play_list[i]
 
-        if minute(play_time) < minute(temp_start) + minute(adv_time):
-            end = play_time
-        else:
-            end = temp_end
-        if answer_score < play_list[minute(end)] - play_list[minute(temp_start)]:
-            answer_score = play_list[minute(end)] - play_list[minute(temp_start)]
-            answer = minute(temp_start)
-        
-        if minute(temp_end) < minute(adv_time):
-            start = 0
-        else:
-            start = minute(temp_end) - minute(adv_time)
-        if answer_score < play_list[minute(temp_end)] - play_list[start]:
-            answer_score = play_list[minute(temp_end)] - play_list[start]
-            answer = minute(temp_end) - minute(start)
-    
-    if answer_score < play_list[minute(play_time)] - play_list[minute(play_time) - minute(adv_time)]:
-        answer_score = play_list[minute(play_time)] - play_list[minute(play_time) - minute(adv_time)]
-        answer = minute(play_time) - minute(adv_time)
     return sec_to_str(answer)
 
 
@@ -66,6 +51,6 @@ def sec_to_str(s):
     answer += str(temp)
     return answer
 
-#print(solution("02:03:55", "00:14:15", ["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"]))
+print(solution("02:03:55", "00:14:15", ["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"]))
 print(solution("99:59:59", "25:00:00", ["69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"]))
-#print(solution("50:00:00", "50:00:00", ["15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"]))
+print(solution("50:00:00", "50:00:00", ["15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"]))
