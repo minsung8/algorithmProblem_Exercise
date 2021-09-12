90775035e34cc4705085b2cec01abd5c729482f6
Kim Min Sung <alstjddl8@naver.com>
2021-09-13 08:19:48 +0900

    Add files via upload

--- programmers009.py
+++ programmers009.py
@@ -1,55 +1,52 @@
 def solution(play_time, adv_time, logs):
-    play_list = [0] * (minute(play_time) + 1)
+
+    play_list = [0 for i in range(str_to_sec(play_time) + 1)]
 
     for log in logs:
         temp_start, temp_end = log.split('-')
-        play_list[minute(temp_start)] += 1
-        play_list[minute(temp_end)] -= 1
-    
-    for i in range(1, len(play_list)):
-        play_list[i] += play_list[i - 1]
-    
-    for i in range(1, len(play_list)):
-        play_list[i] += play_list[i - 1]
+        play_list[str_to_sec(temp_start)] += 1
+        play_list[str_to_sec(temp_end)] -= 1
 
+    for i in range(1, len(play_list)):
+        play_list[i] += play_list[i - 1]
+    for i in range(1, len(play_list)):
+        play_list[i] += play_list[i - 1]
+    
+    answer = 0
+    answer_score = play_list[str_to_sec(adv_time)] - play_list[0]
     logs = sorted(logs)
 
-    answer = 0
-    answer_score = play_list[minute(adv_time)] - play_list[0]
-
-    for i in range(1, len(play_list)):
-        if i + minute(adv_time) <= minute(play_time) and \
-            answer_score < play_list[i + minute(adv_time)] - play_list[i]:
+    for i in range(len(play_list)):
+        
+        if i + str_to_sec(adv_time) <= str_to_sec(play_time) and \
+            answer_score < play_list[i + str_to_sec(adv_time)] - play_list[i]:
             answer = i + 1
-            answer_score = play_list[i + minute(adv_time)] - play_list[i]
+            answer_score = play_list[i + str_to_sec(adv_time)] - play_list[i]
+        
+        if i >= str_to_sec(adv_time) and \
+            answer_score < play_list[i] - play_list[i - str_to_sec(adv_time)]:
+            answer = i - str_to_sec(adv_time) + 1
+            answer_score = play_list[i] - play_list[i - str_to_sec(adv_time)]
 
     return sec_to_str(answer)
 
+def str_to_sec(s):
+    hour, minute, sec = s.split(':')
+    return (int(hour) * 3600) + (int(minute) * 60) + (int(sec))
 
-def minute(t):
-    h1, m1, s1 = t.split(':')
-    return int(h1) * 3600 + int(m1) * 60 + int(s1)
-
-
-def sec_to_str(s):
-    answer = ''
-    temp = str(int(s) // 3600)
-    if len(str(temp)) == 1:
-        temp = '0' + str(temp)
-    answer += str(temp) + ':'
-    s = int(s) % 3600
-
-    temp = str(int(s) // 60)
-    if len(str(temp)) == 1:
-        temp = '0' + str(temp)
-    answer += str(temp) + ':'
-    s = int(s) % 60
-
-    temp = int(s)
-    if len(str(temp)) == 1:
-        temp = '0' + str(temp)
-    answer += str(temp)
-    return answer
+def sec_to_str(t):
+    hour = str(t // 3600)
+    t = t % 3600
+    if len(hour) == 1:
+        hour = '0' + hour
+    minute = str(t // 60)
+    t = t % 60
+    if len(minute) == 1:
+        minute = '0' + minute
+    sec = str(t)
+    if len(sec) == 1:
+        sec = '0' + sec
+    return hour + ':' + minute + ':' + sec
 
 print(solution("02:03:55", "00:14:15", ["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"]))
 print(solution("99:59:59", "25:00:00", ["69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"]))
