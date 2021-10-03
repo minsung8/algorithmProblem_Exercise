2d2d328b44cd28d5b15df32f0ce4e7fc734fc94b
Kim Min Sung <alstjddl8@naver.com>
2021-10-03 14:55:28 +0900

    Add files via upload

--- programmers010.py
+++ programmers010.py
@@ -8,8 +8,8 @@ def solution(word, pages):
     word = word.lower()
 
     for page in pages:
-        url_idx = page.index('content=')
-        temp_url = page[url_idx:].split('"')[1]
+        url_idx = page.index('<meta property="og:url" content=')
+        temp_url = page[url_idx:].split('"')[3]
         url_dic[temp_url].append(idx)
 
         body_start_idx = page.index('<body>')
@@ -33,27 +33,31 @@ def solution(word, pages):
     for key in url_dic.keys():
         temp_str = url_dic[key][1].lower()
         score_dic[key] = 0
-        temp_list = temp_str.split(' ')
-        answer = []
-
-        for temp in temp_list:
-            temp2 = temp.replace('<a', '')
-            temp3 = temp2.replace('</a>', '')
-            new_string = ''.join(filter(str.isalnum, temp3)) 
-            answer.append(new_string)
-        print(answer)
-        score_dic[key] += answer.count(word)
-    print(score_dic)
+        word_list = []
+        temp_word = ''
+        flag = True
+        for i in range(len(temp_str)):
+            if flag and temp_str[i].isalpha():
+                temp_word += temp_str[i]
+            elif flag and not temp_str[i].isalpha() and len(temp_word) > 0:
+                word_list.append(temp_word)
+                temp_word = ''
+            
+            if i > 0 and temp_str[i - 1] == '<':
+                temp_word = ''
+                flag = False
+            
+            if i > 0 and temp_str[i - 1] == 'a' and temp_str[i] == '>':
+                flag = True
+        score_dic[key] += word_list.count(word)
     answer_list = []
     for key in url_dic:
         answer_list.append([key,score_dic[key], url_dic[key][0]]) #[url, url_score, idx]
         for key2 in link_dic[key]:
             answer_list[-1][1] += score_dic[key2] / url_dic[key2][-1]
     answer_list = sorted(answer_list, key=lambda x: (x[1], -x[2]), reverse=True)
-    print(answer_list)
     return url_dic[answer_list[0][0]][0]
 
 
-
-#print(solution('blind', ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\nblind</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]))
+print(solution('blind', ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\nblind</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]))
 print(solution('Muzi', ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://careers.kakao.com/interview/list\"/>\n</head>  \n<body>\n<a href=\"https://programmers.co.kr/learn/courses/4673\"></a>#!MuziMuzi!)jayg07con&&\n\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://www.kakaocorp.com\"/>\n</head>  \n<body>\ncon%\tmuzI92apeach&2<a href=\"https://hashcode.co.kr/tos\"></a>\n\n\t^\n</body>\n</html>"]))
