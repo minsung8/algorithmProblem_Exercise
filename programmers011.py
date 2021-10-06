2b93da2ef9647276c74b5673e4d0ccca930b31e6
Kim Min Sung <alstjddl8@naver.com>
2021-10-07 08:36:14 +0900

    Add files via upload

---
+++ programmers011.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+def solution(s):
+
+    dic = defaultdict(list)
+
+    for x in s:
+        start = x
+        while True:
+            pass
+    answer = []
+    return answer
+
+
+print(solution(["1110","100111100","0111111010"]))
