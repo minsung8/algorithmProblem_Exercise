5cf2901139b8a1e63bd6f995d76851278dcc6caa
Kim Min Sung <alstjddl8@naver.com>
2021-10-11 20:50:46 +0900

    Add files via upload

--- programmers011.py
+++ programmers011.py
@@ -6,10 +6,26 @@ def solution(s):
 
     for x in s:
         start = x
+        i = 0
         while True:
-            pass
-    answer = []
-    return answer
+            if i == len(start) - 2:
+                break
 
+            if start[i:i+3] == '110':
+                temp_str = start[:i] + start[i+3:]
+
+                dic[start].append(start)
+                for j in range(len(temp_str)):
+                    temp_str2 = temp_str[:j] + '110' + temp_str[j:]
+                    dic[start].append(temp_str2)
+                dic[start].append(temp_str + '110')
+
+            i += 1
+    answer = []
+    for key in dic.keys():
+        temp_list = dic[key]
+        answer.append(sorted(temp_list)[0])
+
+    return answer
 
 print(solution(["1110","100111100","0111111010"]))
