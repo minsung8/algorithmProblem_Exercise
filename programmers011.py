b76f22cd243bb3f723bb8418520896d3977fc252
Kim Min Sung <alstjddl8@naver.com>
2021-10-13 08:38:04 +0900

    Add files via upload

--- programmers011.py
+++ programmers011.py
@@ -1,31 +1,17 @@
-from collections import defaultdict
-
 def solution(s):
-
-    dic = defaultdict(list)
-
-    for x in s:
-        start = x
-        i = 0
-        while True:
-            if i == len(start) - 2:
-                break
-
-            if start[i:i+3] == '110':
-                temp_str = start[:i] + start[i+3:]
-
-                dic[start].append(start)
-                for j in range(len(temp_str)):
-                    temp_str2 = temp_str[:j] + '110' + temp_str[j:]
-                    dic[start].append(temp_str2)
-                dic[start].append(temp_str + '110')
-
-            i += 1
     answer = []
-    for key in dic.keys():
-        temp_list = dic[key]
-        answer.append(sorted(temp_list)[0])
-
+    for temp_s in s:
+        temp_answer = temp_s
+        i = 0
+        while i < len(temp_answer):
+            if temp_s[i:i+3] == '110':
+                front = temp_s[:i]
+                if len(front) > 2:
+                    for j in range(len(front) - 2):
+                        if front[j:j+3] > '110':
+                            temp_answer = front[:j] + '110' + front[j:len(front)] + temp_s[i+3:]
+            i += 1
+        answer.append(temp_answer)
     return answer
 
 print(solution(["1110","100111100","0111111010"]))
