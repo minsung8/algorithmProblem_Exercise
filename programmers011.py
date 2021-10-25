696fa735ce3ba419f5aa3d60fa492effe648c3c4
Kim Min Sung <alstjddl8@naver.com>
2021-10-25 21:47:49 +0900

    Add files via upload

--- programmers011.py
+++ programmers011.py
@@ -1,17 +1,6 @@
 def solution(s):
     answer = []
-    for temp_s in s:
-        temp_answer = temp_s
-        i = 0
-        while i < len(temp_answer):
-            if temp_s[i:i+3] == '110':
-                front = temp_s[:i]
-                if len(front) > 2:
-                    for j in range(len(front) - 2):
-                        if front[j:j+3] > '110':
-                            temp_answer = front[:j] + '110' + front[j:len(front)] + temp_s[i+3:]
-            i += 1
-        answer.append(temp_answer)
     return answer
 
 print(solution(["1110","100111100","0111111010"]))
+#["1101","100110110","0110110111"]
