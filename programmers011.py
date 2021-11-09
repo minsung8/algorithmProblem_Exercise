adfac55aec7ae50b97abcec1a2cf6ad992961ef1
Kim Min Sung <alstjddl8@naver.com>
2021-11-09 21:24:16 +0900

    Add files via upload

--- programmers011.py
+++ programmers011.py
@@ -2,24 +2,31 @@ def solution(s):
 
     answer = []
 
-    for temp_s in s: #
-
-        i = 0
-        temp_list = list(temp_s)
-
-        while i < len(temp_list):
-            if i > 1 and temp_list[i - 2:i + 1] == ['1', '1', '1']:
-                temp_list2 = temp_list[i - 1:]
-                if '110' not in "".join(temp_list2):
+    for temp_s in s: 
+        temp_list = []
+        temp_cnt = 0
+        
+        for s in temp_s:
+            if s == '0':
+                if temp_list[-2:] == ['1', '1']:
+                    temp_list.pop(-1)
+                    temp_list.pop(-1)
+                    temp_cnt += 1
+                else:
+                    temp_list.append(s)
+            else:
+                temp_list.append(s)
+        if '0' not in temp_list:
+            temp_answer = ('110' * temp_cnt) + "".join(temp_list)
+        else:
+            j = len(temp_list) - 1
+            while j > -1:
+                if temp_list[j] == '0':
+                    temp_answer = "".join(temp_list[:j + 1]) + ('110' * temp_cnt) + "".join(temp_list[j+1:])
                     break
-                change_idx = "".join(temp_list2).index('110') + 1 + i
-                temp_list[i] = '0'
-                temp_list[change_idx] = '1'
-                
-            i += 1
-
-        answer.append("".join(temp_list))
-
+                j -= 1
+        
+        answer.append(temp_answer)
     return answer
 
 #print(solution(["1110"]))
