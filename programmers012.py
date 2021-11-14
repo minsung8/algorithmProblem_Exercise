3f07d6bd623ae85d9ac9ea03159eb8fe62d115db
Kim Min Sung <alstjddl8@naver.com>
2021-11-14 22:09:51 +0900

    Add files via upload

--- programmers012.py
+++ programmers012.py
@@ -1,8 +1,31 @@
+answer_list = []
+
 def solution(sticker):
-    answer = 0
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
+    global answer_list
+    
+    first_list = sticker[2:-1]
+    second_list = sticker[3:]
+    dfs(sticker[0], first_list)
+    dfs(sticker[1], second_list)
 
-    return answer
+    return answer_list
 
+def dfs(sum, temp_list):
+
+
+    if len(temp_list) == 2:
+        answer_list.append(sum + max(temp_list))
+    elif len(temp_list) == 3:
+        answer_list.append(sum + max(temp_list[1], temp_list[0] + temp_list[2]))
+    else:
+        first_list = temp_list[2:]
+        second_list = temp_list[3:]
+        dfs(temp_list[0] + sum, first_list)
+        dfs(temp_list[1] + sum, second_list)
+    return
+
+
+
+print(solution([14, 6, 5, 11, 3, 9, 2, 10]	)) # 36
+print(solution([1, 3, 2, 5, 4])) # 8
