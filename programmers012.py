7126ff616505b479a91496b8af0526e8bdaa5863
Kim Min Sung <alstjddl8@naver.com>
2021-11-15 21:23:28 +0900

    Add files via upload

--- programmers012.py
+++ programmers012.py
@@ -1,31 +1,24 @@
-answer_list = []
-
 def solution(sticker):
 
-    global answer_list
-    
-    first_list = sticker[2:-1]
-    second_list = sticker[3:]
-    dfs(sticker[0], first_list)
-    dfs(sticker[1], second_list)
+    answer = 0
 
-    return answer_list
+    if len(sticker) < 4:
+        return max(sticker)
 
-def dfs(sum, temp_list):
-
-
-    if len(temp_list) == 2:
-        answer_list.append(sum + max(temp_list))
-    elif len(temp_list) == 3:
-        answer_list.append(sum + max(temp_list[1], temp_list[0] + temp_list[2]))
+    if sticker[0] < sticker[1] + sticker[-1]:
+        sticker[0] = 0
+        i = 1
     else:
-        first_list = temp_list[2:]
-        second_list = temp_list[3:]
-        dfs(temp_list[0] + sum, first_list)
-        dfs(temp_list[1] + sum, second_list)
-    return
+        sticker[1] = 0
+        sticker[-1] = 0
+        i = 2
+    
+    while i < len(sticker):
+        pass
+    print(sticker)
 
 
 
 print(solution([14, 6, 5, 11, 3, 9, 2, 10]	)) # 36
-print(solution([1, 3, 2, 5, 4])) # 8
+#print(solution([1, 3, 2, 5, 4])) # 8
+print(solution([5, 1, 16, 17, 16] ))
