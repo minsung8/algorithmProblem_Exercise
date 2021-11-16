7f67c4f666253955de81f9e78f2194f09ccc0fc0
Kim Min Sung <alstjddl8@naver.com>
2021-11-16 21:46:08 +0900

    Add files via upload

--- programmers012.py
+++ programmers012.py
@@ -5,31 +5,21 @@ def solution(sticker):
     if len(sticker) < 4:
         return max(sticker)
     
-    for i in range(len(sticker) - 1):
-        if sticker[i] == 0:
-            continue
-
-        if i == len(sticker) - 2:
-            if sticker[i] < sticker[i + 1]:
-                sticker[i] = 0
+    for i in range(1, len(sticker)):
+        if i == 1:
+            if sticker[i] + sticker[-1] < sticker[i - 1]:
+                sticker[i] = sticker[i - 1]
             else:
-                sticker[i + 1] = 0
-            break
-        
-        if i == 0 and sticker[i] < sticker[i + 1] + sticker[-1]:
-            sticker[i] = 0
+                sticker[i - 1] = 0
+            
         else:
-
-            if sticker[i] + sticker[i + 2] < sticker[i + 1]:
-                sticker[i] = 0
-                sticker[i + 2] = 0
+            if sticker[i] + sticker[i - 2] < sticker[i - 1]:
+                sticker[i] = sticker[i - 1]
             else:
-                sticker[i + 1] = 0
-
-    return sum(sticker)
-
+                sticker[i] += sticker[i - 2]
+    return sticker[-1]
 
 
 print(solution([14, 6, 5, 11, 3, 9, 2, 10]	)) # 36
 print(solution([1, 3, 2, 5, 4])) # 8
-print(solution([1, 100, 1, 1, 100] ))
+print(solution([1, 16, 1, 17, 16] ))
