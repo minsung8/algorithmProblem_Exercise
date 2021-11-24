940cd3d0c0d059a8fc1d299b43bddb1ccd6243d2
Kim Min Sung <alstjddl8@naver.com>
2021-11-24 21:13:29 +0900

    Add files via upload

--- programmers013.py
+++ programmers013.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def solution(a, edges):
 
     if sum(a) > 0:
@@ -11,32 +13,13 @@ def solution(a, edges):
     else:
         index = a.index(_max)
 
-    temp_list = [[index, 1]]
-    answer = 0
-    visited = [0] * len(edges)
- 
-    while temp_list:
-        temp, temp_score = temp_list.pop(0)
-        next_score = temp_score + 1
+    dic = defaultdict(list)
 
-        i = 0
-        while i < len(edges):
-            
-            if edges[i][0] == temp:
-                answer += temp_score * a[edges[i][1]]
-                temp_list.append([edges[i][1], next_score])
-                edges.pop(i)
-                
-            elif edges[i][1] == temp:
-                answer += temp_score * a[edges[i][0]]
-                temp_list.append([edges[i][0], next_score])
-                edges.pop(i)    
-            i += 1
-            print(edges)
+    for edge in edges:
+        dic[edge[0]].append(edge[1])
+        dic[edge[1]].append(edge[0])
 
-        print(temp_list, answer)
-        
-    return answer
+    return dic
 
 print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
 print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
