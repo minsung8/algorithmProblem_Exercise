e416aeaf26f79cace4b93796a9cf76574bea1388
Kim Min Sung <alstjddl8@naver.com>
2021-11-29 21:42:55 +0900

    Add files via upload

--- programmers013.py
+++ programmers013.py
@@ -2,25 +2,16 @@ from collections import defaultdict
 
 def solution(a, edges):
 
-    if sum(a) > 0:
-        return - 1
-
-    _min = min(a)
-    _max = max(a)
-
-    if abs(_min) > abs(_max):
-        index = a.index(_min)
-    else:
-        index = a.index(_max)
+    if sum(a) != 0:
+        return -1 
 
     dic = defaultdict(list)
 
-    for edge in edges:
-        dic[edge[0]].append(edge[1])
-        dic[edge[1]].append(edge[0])
+    for x, y in edges:
+        dic[x].append(y)
+        dic[y].append(x)
 
-    return dic
+    return dic.values()
 
 print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
-print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
-##
+#print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
