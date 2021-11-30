4968ffc8881052c2c0c711ada8867a9c6a6f290a
Kim Min Sung <alstjddl8@naver.com>
2021-11-30 20:38:10 +0900

    Add files via upload

--- programmers013.py
+++ programmers013.py
@@ -11,7 +11,24 @@ def solution(a, edges):
         dic[x].append(y)
         dic[y].append(x)
 
-    return dic.values()
+    temp_list = sorted(list(dic.keys()), key=lambda x: len(dic[x]))
+    visited = []
+    answer = 0
+
+    for temp in temp_list:
+        dif = a[temp]
+        for i in range(len(dic[temp])):
+            if dic[temp][i] not in visited:
+                a[dic[temp][i]] += dif
+                a[temp] = 0
+                visited.append(temp)
+                answer += abs(dif)
+                break
+    
+    for temp in a:
+        if temp != 0:
+            return -1
+    return answer
 
 print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
-#print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
+print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
