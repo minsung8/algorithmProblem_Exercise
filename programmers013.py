1549bc26bacbc871fdbb1cb720016064e21c443d
Kim Min Sung <alstjddl8@naver.com>
2021-12-03 21:09:16 +0900

    Add files via upload

--- programmers013.py
+++ programmers013.py
@@ -5,33 +5,35 @@ def solution(a, edges):
     if sum(a) != 0:
         return -1 
 
-    dic = defaultdict(list)
+    graph = defaultdict(list)
 
     for x, y in edges:
-        dic[x].append(y)
-        dic[y].append(x)
+        graph[x].append(y)
+        graph[y].append(x)
 
-    temp_list = sorted(list(dic.keys()), key=lambda x: len(dic[x]))
-    visited = {}
-    for i in range(len(a)):
-        visited[i] = 0
+    len_graph = {}
+
+    for key in graph.keys():
+        len_graph[key] = len(graph[key])
+
+    print(len_graph)
+
     answer = 0
-    temp_list.pop(-1)
-    for temp in temp_list:
-        dif = a[temp]
-        if a[temp] != 0:
-            for i in range(len(dic[temp])):
-                if visited[dic[temp][i]] == 0:
-                    a[dic[temp][i]] += dif
-                    a[temp] = 0
-                    answer += abs(dif)
-                    break
-        visited[temp] = 1
+    
+    for temp in edges:
+        if len_graph[temp[0]] > len_graph[temp[1]]:
+            a[temp[0]] += a[temp[1]]
+            answer += abs(a[temp[1]])
+            a[temp[1]] = 0
 
-    for temp in a:
-        if temp != 0:
-            return -1
+        elif len_graph[temp[0]] < len_graph[temp[1]]:
+            a[temp[1]] += a[temp[0]]
+            answer += abs(a[temp[0]])
+            a[temp[0]] = 0
+
     return answer
+
+
 
 print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
 print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
