aa37f6b1276be40c96899a8dae3906ab54fe4a93
Kim Min Sung <alstjddl8@naver.com>
2021-12-04 13:21:16 +0900

    Add files via upload

--- programmers013.py
+++ programmers013.py
@@ -16,24 +16,10 @@ def solution(a, edges):
     for key in graph.keys():
         len_graph[key] = len(graph[key])
 
-    print(len_graph)
 
-    answer = 0
-    
-    for temp in edges:
-        if len_graph[temp[0]] > len_graph[temp[1]]:
-            a[temp[0]] += a[temp[1]]
-            answer += abs(a[temp[1]])
-            a[temp[1]] = 0
-
-        elif len_graph[temp[0]] < len_graph[temp[1]]:
-            a[temp[1]] += a[temp[0]]
-            answer += abs(a[temp[0]])
-            a[temp[0]] = 0
-
-    return answer
+    return graph, len_graph
 
 
 
 print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
-print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
+#print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
