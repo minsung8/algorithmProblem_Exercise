271c4c497ea9171e51cc84507d3335efbc87c0b9
Kim Min Sung <alstjddl8@naver.com>
2021-12-06 21:07:21 +0900

    Add files via upload

--- programmers013.py
+++ programmers013.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 def solution(a, edges):
+    answer = 0
 
     if sum(a) != 0:
         return -1 
@@ -11,15 +12,26 @@ def solution(a, edges):
         graph[x].append(y)
         graph[y].append(x)
 
-    len_graph = {}
+    end_node_list = []
 
     for key in graph.keys():
-        len_graph[key] = len(graph[key])
+        if len(graph[key]) == 1:
+            end_node_list.append(key)
+    print(end_node_list)
+    while end_node_list:
+        temp = end_node_list.pop(0)
+        if not graph[temp]:
+            continue
+        next_node = graph[temp][0]
+        a[next_node] += a[temp]
+        answer += abs(a[temp])
 
+        graph[next_node].remove(temp)
 
-    return graph, len_graph
+        if len(graph[next_node]) == 1:
+            end_node_list.append(next_node)
 
-
+    return answer
 
 print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
 #print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
