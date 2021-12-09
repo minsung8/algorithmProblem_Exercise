64ec853ac91b6ca3f6fbd88f817ba03927412c17
Kim Min Sung <alstjddl8@naver.com>
2021-12-09 20:44:42 +0900

    Add files via upload

--- programmers013.py
+++ programmers013.py
@@ -1,32 +1,39 @@
 from collections import defaultdict
+visited = 0
+graph = defaultdict(list)
+answer = 0
+import sys
+sys.setrecursionlimit(300000)
 
 def solution(a, edges):
-    answer = 0
-
+    global visited
+    global graph
+    global answer
     if sum(a) != 0:
-        return -1 
-
-    graph = defaultdict(list)
+        return -1
 
     for x, y in edges:
         graph[x].append(y)
         graph[y].append(x)
     
-    key_list = list(graph.keys())
+    visited = [0] * len(a)
 
-    while key_list:
-        key = key_list.pop(0)
-        if len(graph[key]) == 1:
-            temp_value = graph[key].pop(0)
-            answer += abs(a[key])
-            a[temp_value] += a[key]
-            a[key] = 0
-            graph[temp_value].remove(key)
-        else:
-            key_list.append(key)
+    dfs(0, a)
 
+    return answer
 
-    return graph, a, answer, edges, graph.keys()
+def dfs(start, a):
+    global visited
+    global graph
+    global answer
+    key_list = graph[start]
+
+    for v in key_list:
+        if visited[v] == 0:
+            dfs(v, a)
+            answer += abs(a[v])
+            
+    return 
 
 print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
 print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
