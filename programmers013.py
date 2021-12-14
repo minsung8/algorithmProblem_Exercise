0d078d7b00da4404dc1c5fdef7c7f2fc24fd6a6e
Kim Min Sung <alstjddl8@naver.com>
2021-12-14 21:00:38 +0900

    Add files via upload

--- programmers013.py
+++ programmers013.py
@@ -28,18 +28,10 @@ def solution(a, edges):
 def dfs(key, graph, a, temp_answer):
     global answer, visited
 
-    for value in graph[key]:
-        temp_len = len(graph[value])
-        if visited[value] == 0 and temp_len != 1:
-            visited[key] = 1
-            dfs(value, graph, a, temp_answer + abs(a[key]))
-        elif visited[value] == 0 and temp_len == 1:
-            temp_answer += abs(a[value])
-            visited[value] = 1
-            continue
     
-    answer += temp_answer
     
+    
+    answer += abs(temp_answer)
     return
 
 print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
