4c42ff0251e5356b044c2a1d66579996e6bc51fa
Kim Min Sung <alstjddl8@naver.com>
2021-12-15 20:40:37 +0900

    Add files via upload

--- programmers013.py
+++ programmers013.py
@@ -19,20 +19,24 @@ def solution(a, edges):
         graph[x].append(y)
         graph[y].append(x)
 
-    for key in graph.keys():
-        if len(graph[key]) == 1 and visited[key] == 0:
-            dfs(key, graph, a, 0)
+    dfs(0, graph, a)
         
     return answer
 
-def dfs(key, graph, a, temp_answer):
+def dfs(key, graph, a):
+
     global answer, visited
 
+    now = a[key]
+    visited[key] = 1
+
+    for value in graph[key]:
+        if visited[value] == 0: ## 방문하지 않았으면 
+            now += dfs(value, graph, a)
     
-    
-    
-    answer += abs(temp_answer)
-    return
+    answer += abs(now)
+
+    return now
 
 print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
 #print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
