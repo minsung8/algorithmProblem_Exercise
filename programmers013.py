f34e49f978cbf2925f6bfc30692fa2754bc149ca
Kim Min Sung <alstjddl8@naver.com>
2021-12-19 19:55:05 +0900

    Add files via upload

--- programmers013.py
+++ programmers013.py
@@ -1,42 +1,43 @@
 from collections import defaultdict
 visited = []
+graph = defaultdict(list)
 answer = 0
 import sys
 sys.setrecursionlimit(300000)
 
 def solution(a, edges):
 
-    global answer, visited
-
-    visited = [0] * len(a)
-
     if sum(a) != 0:
         return -1
 
-    graph = defaultdict(list)
-    
-    for x, y in edges:
-        graph[x].append(y)
-        graph[y].append(x)
 
-    dfs(0, graph, a)
-        
+    global visited, graph, answer
+    visited = [0] * len(a)
+    answer = 0 
+    graph = defaultdict(list)
+
+    for edge in edges:
+        graph[edge[0]].append(edge[1])
+        graph[edge[1]].append(edge[0])
+
+    dfs(0, a)
+
     return answer
 
-def dfs(key, graph, a):
+def dfs(start, a):
+    global visited, graph, answer
 
-    global answer, visited
+    visited[start] = 1
+    temp_answer = a[start]
 
-    now = a[key]
-    visited[key] = 1
+    for value in graph[start]:
+        if visited[value] == 0:
+            temp_answer += dfs(value, a)
 
-    for value in graph[key]:
-        if visited[value] == 0: ## 방문하지 않았으면 
-            now += dfs(value, graph, a)
-    
-    answer += abs(now)
+    answer += abs(temp_answer)
 
-    return now
+    return temp_answer
+
 
 print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
-#print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
+print(solution([0, 1, 0], [[0,1],[1,2]])) # -1
