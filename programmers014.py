cba3a69f6737a376ba2b90431551787cfb325308
Kim Min Sung <alstjddl8@naver.com>
2021-12-24 23:57:03 +0900

    Add files via upload

--- programmers014.py
+++ programmers014.py
@@ -1,40 +1,17 @@
+from collections import defaultdict
+
 def solution(a, b, g, s, w, t):
-    answer = 0
+
+    dic = defaultdict(list)
 
     for i in range(len(g)):
-        temp_w = w[i]
-        temp_t = t[i]
-        temp_g = g[i]
-        temp_s = s[i]
-        while True:
-            
-            now = [0, 0]        # 수레 (금, 은) 
-            print(a, b)
-            answer += temp_t
-            if a >= temp_w:     # 금 담기 
-                a -= temp_w
-                now[0] += temp_w
-            else:
-                now[0] += a
-                a = 0
-            
-            if now[0] < temp_w:
-                temp_w_2 = temp_w - now[0]
-                if b >= temp_w_2:     # 은 담기 
-                    b -= temp_w
-                    now[1] += temp_w_2
-                else:
-                    now[1] += b
-                    b = 0
-            
-            if a == 0 and b == 0:
-                break
+        dic[i].append(g[i])
+        dic[i].append(s[i])
+        dic[i].append(w[i])
+        dic[i].append(t[i])
 
-            temp_g -= now[0]    # 채우기 
-            temp_s -= now[1]
-            answer += temp_t
-        print(answer)
-    return answer
+    dic = sorted(dic, key=lambda x: dic[x][2] // dic[x][3], reverse=True)
+    return dic
 
 #print(solution(10, 10, [100], [100], [7], [10]))        #10
 print(solution(90, 500, [70, 70, 0], [0, 0, 500], [100, 100, 2], [4, 8, 1]))        #10
