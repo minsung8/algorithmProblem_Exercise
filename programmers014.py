15b6bc3a98385b3fdce609bffcbb875d52f5dc01
Kim Min Sung <alstjddl8@naver.com>
2022-01-04 20:45:12 +0900

    Add files via upload

--- programmers014.py
+++ programmers014.py
@@ -5,14 +5,10 @@ def solution(a, b, g, s, w, t):
     dic = defaultdict(list)
 
     for i in range(len(g)):
-        dic[i].append(g[i])
-        dic[i].append(s[i])
-        dic[i].append(w[i])
-        dic[i].append(t[i])
+        pass
 
-    key_list = sorted(dic, key=lambda x: dic[x][3], reverse=True)   # 1 -> 4 -> 8
+    answer = -1
+    return answer
 
-    return dic
 
-#print(solution(10, 10, [100], [100], [7], [10]))        #10
-print(solution(90, 500, [70, 70, 0], [0, 0, 500], [100, 100, 2], [4, 8, 1]))        #10
+print(solution(90, 500, [70, 70, 0], [0, 0, 500], [100, 100, 2], [4, 8, 1]))
