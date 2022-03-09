751a1fbbbede2e81da687f29977386e5bcfe589c
Kim Min Sung <alstjddl8@naver.com>
2022-03-09 21:08:46 +0900

    Add files via upload

--- programmers016.py
+++ programmers016.py
@@ -1,30 +1,23 @@
 
-from django.dispatch import receiver
-
-
 def solution(n, info):
-    score_list = []
 
-    for i in range(len(info)):
-        score_list.append([ i, ( 2 * (10 - i) / (info[i] + 1) ) + (10 - i)])
-    
+    temp = [0] * len(info)
 
-    return sorted(score_list, key=lambda x : (x[1], -x[0]), reverse=True)
+    dfs(0, info, temp, n)
 
+    return
+
+def dfs(i, info, temp, n):
+
+    # 승리
+    temp_score = info[i] + 1
+    temp_v = temp.copy()[i] = temp_score
+    n_v = n - temp_score
+    if n_v > 0:
+        dfs(i + 1, info, temp_v, n_v)
+
+    # 패배
+    dfs(i + 1, info, temp, n)
+    return
 
 print(solution(5, [2,1,1,1,0,0,0,0,0,0,0]))     # res = [0,2,2,0,1,0,0,0,0,0,0]
-
-# 3 - > 10 = 3.3
-# 2 -> 9 = 4.5
-# 2 -> 8 = 4
-# 1 -> 7 = 7  
-# 2 -> 6 = 3
-# [3.3333333333333335, 4.5, 4.0, 3.5, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0]
-
-
-# [0 -10], [3, 10] => 3,10 = 13.3
-# [0 -9], [2, 9] => 2,18 = 18
-# [0 -8], [2, 8] => 2,16 = 16
-# [0, -7], [2, 7] => 2,7 = 14
-# [0, -6], [1, 6] => 1, 6 = 12
-# [0, -5], [1, 5] => 1, 5 = 15
