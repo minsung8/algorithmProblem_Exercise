f3e1a5dc3e3789411566eaa13d397e6ab83d7d2c
Kim Min Sung <alstjddl8@naver.com>
2022-07-31 15:08:39 +0900

    Add files via upload

--- programmers017.py
+++ programmers017.py
@@ -2,39 +2,24 @@ def solution(a, b, g, s, w, t):
 
     min_t = 0
     max_t = (10**9) * (10**5) * 4
-    
+
     ## 이분탐색
     while True:
-        temp_a = a
-        temp_b = b
-
+        gold = 0
+        silver = 0
+        total = 0
         temp_t = (min_t + max_t) // 2
 
         for i in range(len(g)):
 
-            temp_cnt = int(((temp_t / t[i]) + 1) / 2)
-            temp_w = w[i] * temp_cnt
-            
-            if temp_a > 0 and g[i] > 0 and temp_w > 0:
-                temp = 0
-                if temp_a > g[i]:
-                    temp = min(g[i], temp_w)
-                else:
-                    temp = min(temp_a, temp_w)
-                temp_w -= temp
-                temp_a -= temp
+            move_cnt = int(((temp_t / t[i]) + 1) / 2)
 
-            if temp_b > 0 and s[i] > 0 and temp_w > 0:
-                temp = 0
-                if temp_b > s[i]:
-                    temp = min(s[i], temp_w)
-                else:
-                    temp = min(temp_b, temp_w)
-                temp_w -= temp
-                temp_b -= temp
+            gold += g[i] if (g[i] < move_cnt * w[i]) else move_cnt * w[i] 
+            silver += s[i] if (s[i] < move_cnt * w[i]) else move_cnt * w[i]
+            total += g[i] + s[i] if (g[i] + s[i] < move_cnt * w[i]) else move_cnt * w[i] 
 
         # 미션 클리어
-        if temp_a <= 0 and temp_b <= 0:
+        if gold >= a and silver >= b and total >= a + b:
             max_t = temp_t
         else:
             min_t = temp_t
