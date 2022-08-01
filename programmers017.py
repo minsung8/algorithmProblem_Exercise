13d63ba6ce0cb2b70e14ba64e663cabfde175c26
Kim Min Sung <alstjddl8@naver.com>
2022-08-01 20:03:30 +0900

    Add files via upload

--- programmers017.py
+++ programmers017.py
@@ -1,33 +1,35 @@
 def solution(a, b, g, s, w, t):
 
-    min_t = 0
-    max_t = (10**9) * (10**5) * 4
+    start = 0
+    end = (10**9) * (10**5) * 4
 
-    ## 이분탐색
     while True:
-        gold = 0
-        silver = 0
-        total = 0
-        temp_t = (min_t + max_t) // 2
+
+        mid = int((start + end) / 2)
+        gold = 0    # 캘 수 있는 금
+        silver = 0  # 캘 수 있는 은
+        total = 0   # 캘 수  있는 금  + 은
 
         for i in range(len(g)):
-
-            move_cnt = int(((temp_t / t[i]) + 1) / 2)
-
-            gold += g[i] if (g[i] < move_cnt * w[i]) else move_cnt * w[i] 
-            silver += s[i] if (s[i] < move_cnt * w[i]) else move_cnt * w[i]
+            
+            move_cnt = int((mid / t[i] + 1)/ 2)
+            
+            gold += g[i] if g[i] < move_cnt * w[i] else move_cnt * w[i]
+            silver += s[i] if s[i] < move_cnt * w[i] else move_cnt * w[i]
             total += g[i] + s[i] if (g[i] + s[i] < move_cnt * w[i]) else move_cnt * w[i] 
-
-        # 미션 클리어
+        
         if gold >= a and silver >= b and total >= a + b:
-            max_t = temp_t
+            end = mid
         else:
-            min_t = temp_t
-
-        if min_t >= max_t - 1: 
+            start = mid
+        
+        if start >= end - 1: 
             break
 
-    return max_t
+    return end
 
 print(solution(10, 10, [100], [100], [7], [10]))    # 50
 print(solution(90, 500, [70, 70, 0], [0, 0, 500], [100, 100, 2], [4, 8, 1]))    # 50
+
+# 50 = (2n - 1) * t 
+# (50 / t + 1)/ 2 = 2n
