46b2153303083a2a304a3797803f7f7c9099c200
Kim Min Sung <alstjddl8@naver.com>
2022-08-03 20:06:41 +0900

    Add files via upload

--- programmers017.py
+++ programmers017.py
@@ -4,20 +4,19 @@ def solution(a, b, g, s, w, t):
     end = (10**9) * (10**5) * 4
 
     while True:
-
         mid = int((start + end) / 2)
-        gold = 0    # 캘 수 있는 금
-        silver = 0  # 캘 수 있는 은
-        total = 0   # 캘 수  있는 금  + 은
+
+        gold = 0
+        silver = 0
+        total = 0
 
         for i in range(len(g)):
-            
+
             move_cnt = int((mid / t[i] + 1)/ 2)
-            
-            gold += g[i] if g[i] < move_cnt * w[i] else move_cnt * w[i]
-            silver += s[i] if s[i] < move_cnt * w[i] else move_cnt * w[i]
-            total += g[i] + s[i] if (g[i] + s[i] < move_cnt * w[i]) else move_cnt * w[i] 
-        
+            gold += min(move_cnt * w[i], g[i])
+            silver += min(move_cnt * w[i], s[i])
+            total += min(g[i] + s[i], move_cnt * w[i])
+
         if gold >= a and silver >= b and total >= a + b:
             end = mid
         else:
@@ -30,6 +29,3 @@ def solution(a, b, g, s, w, t):
 
 print(solution(10, 10, [100], [100], [7], [10]))    # 50
 print(solution(90, 500, [70, 70, 0], [0, 0, 500], [100, 100, 2], [4, 8, 1]))    # 50
-
-# 50 = (2n - 1) * t 
-# (50 / t + 1)/ 2 = 2n
