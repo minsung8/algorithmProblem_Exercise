23f7cca55044a25e27851044409395a0756b2bd4
Kim Min Sung <alstjddl8@naver.com>
2022-08-15 17:30:02 +0900

    Add files via upload

--- HackerRank/p007.py
+++ HackerRank/p007.py
@@ -1,24 +1,35 @@
-# https://www.hackerrank.com/challenges/magic-square-forming/problem?isFullScreen=true
+# https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem?isFullScreen=true
 
-def simpleArraySum(arr):
+def simpleArraySum(ranked, player):
     # Write your code here
+
+    ranked_set_list = sorted(list(set(ranked)), reverse=True)
+    all_list = sorted(list(set(ranked + player)), reverse=True)
+    answer_list = []
+    for i in range(len(all_list)):
+        if all_list[i] in ranked_set_list:
+            answer_list.append([all_list[i], ranked_set_list.index(all_list[i]) + 1])
+        else:
+            answer_list.append([all_list[i], -1])
+
+    rank_dic = {}
+    for i in range(len(answer_list) - 1):
+        if answer_list[i][1] == -1:
+            rank_dic[answer_list[i][0]] = answer_list[i + 1][1]
+        else:
+            rank_dic[answer_list[i][0]] = answer_list[i][1]
+    if answer_list[-1][1] == -1:
+        rank_dic[answer_list[-1][0]] = len(ranked_set_list) + 1
+    else:
+        rank_dic[answer_list[-1][0]] = answer_list[-1][1]
+
     answer = []
-    poss = []
-
-    for i in range(1, 10):
-        for j in range(1, 10):
-            if i != j and set([i, 15 - (i + j), j, 5 + j - i, 5, 5 - j + i, 10 - j, i + j - 5, 10 - i]) == set(range(1,10)):
-                poss.append([i, 15 - (i + j), j, 5 + j - i, 5, 5 - j + i, 10 - j, i + j - 5, 10 - i])
-    
-    temp_list = arr[0] + arr[1] + arr[2]
-    for temp in poss:
-        temp_answer = 0
-        for i in range(len(temp_list)):
-            temp_answer += abs(temp_list[i] - temp[i])
-        answer.append(temp_answer)
-
-    
+    for p in player:
+        answer.append(rank_dic[p])
     return answer
-    
 
-print(simpleArraySum([[4, 9, 2], [3, 5, 7], [8, 1, 5]]))
+    
+print(simpleArraySum([100, 100, 50, 40, 40, 20, 10], [5, 25, 50, 120]))     # 6 4 2 1
+
+# 120 100 50 40 25 20 10 5
+#      1   2  3     4  5 
