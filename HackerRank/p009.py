849bd9c10f12382794765b5e4cb6ab5629b061fa
Kim Min Sung <alstjddl8@naver.com>
2022-08-16 22:42:14 +0900

    Add files via upload

---
+++ HackerRank/p009.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/non-divisible-subset/problem?isFullScreen=true
+
+from itertools import combinations
+
+def solution(k, s):
+
+
+    return 
+
+
+print(solution(3, [1, 7, 2, 4]))    # 3
+print(solution(7, [278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436]))
