b0b924ae6dad99e95959cf64d072398b1d55645f
Kim Min Sung <alstjddl8@naver.com>
2022-08-18 22:37:46 +0900

    Add files via upload

--- HackerRank/p009.py
+++ HackerRank/p009.py
@@ -4,8 +4,26 @@ from itertools import combinations
 
 def solution(k, s):
 
+    start = len(s)
+    answer = 0
 
-    return 
+    while start > 0:
+        
+        temp_list = combinations(s, start)
+        flag = True
+        
+        for t in temp_list:
+            temp_list2 = combinations(t, 2)
+            for t2 in temp_list2:
+                if sum(t2) % k == 0:
+                    flag = False
+                    break
+            if flag:
+                return start
+            flag = True
+        start -= 1
+
+    return 0
 
 
 print(solution(3, [1, 7, 2, 4]))    # 3
