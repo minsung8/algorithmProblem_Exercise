88e005aaf5ac04f08f71f1da12c9b447df5caf41
Kim Min Sung <alstjddl8@naver.com>
2022-08-21 18:55:50 +0900

    Add files via upload

--- HackerRank/p009.py
+++ HackerRank/p009.py
@@ -1,37 +1,32 @@
 # https://www.hackerrank.com/challenges/non-divisible-subset/problem?isFullScreen=true
 
-from itertools import combinations
-from collections import defaultdict
 def solution(k, s):
 
-    dic = defaultdict(list)
-    answer = []
-    for i in range(len(s)):
-        for j in range(i + 1, len(s)):
-            if (s[i] + s[j]) % k != 0:
-                dic[s[i]].append(s[j])
-                dic[s[j]].append(s[i])
-    
-    for a in dic.keys():
-        answer.append(check(dic[a], k))
-        
+    dic = {}
+    answer = 0
+    for i in s:
+        temp = i % k
+        if temp in dic: dic[temp] += 1
+        else: dic[temp] = 1
+
+    for i in range(((k + 1) // 2), k):
+        temp = k - i
+
+        if i == temp and i in dic:
+            answer += 1
+            continue
+
+        if i in dic and temp in dic:
+            answer += max(dic[i], dic[temp])
+        elif i in dic:
+            answer += dic[i]
+        elif j in dic:
+            answer += dic[j]
+
     return answer
-
-
-def check(s, k):
-
-    dic = defaultdict(list)
-    for i in range(len(s)):
-        for j in range(i + 1, len(s)):
-            if (s[i] + s[j]) % k != 0:
-                dic[s[i]].append(s[j])
-                dic[s[j]].append(s[i])
-    
-    print(len(dic.keys()))
-
-    return 
-
 
 print(solution(3, [1, 7, 2, 4]))    # 3
 print(solution(7, [278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436]))
 
+# 2 6 3 2 2
+# 6 3 2 
