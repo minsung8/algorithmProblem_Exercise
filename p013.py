e216bf003331faa3f2202b3104ed0a4ed610d27f
Kim Min Sung <alstjddl8@naver.com>
2022-09-04 00:13:16 +0900

    Add files via upload

--- p013.py
+++ p013.py
@@ -1,49 +1,31 @@
 # https://www.hackerrank.com/challenges/bigger-is-greater/problem?isFullScreen=true
 
-
 def biggerIsGreater(w):
-    if w == "".join(sorted(list(w), reverse=True)):
-        return "no answer"
 
-    reversed_w = w[::-1]
+    s = list(w[::-1])
 
-    # temp = []
-    # for i in w:
-    #     temp.append(ord(i))
-    # print(temp)
-
-    for i in range(len(reversed_w) - 1):
-        for j in range(i + 1, len(reversed_w) - 1):
-            if reversed_w[i] > reversed_w[j]:
-                answer = ''
-                for k in range(len(reversed_w)):
-                    if i == k:
-                        answer += reversed_w[j]
-                    elif j == k:
-                        answer += reversed_w[i]
-                    else:
-                        answer += reversed_w[k]
-                answer = answer[::-1]
-                return answer[:len(w) - j] + "".join(sorted(list(answer[len(w) - j:])))
+    for i in range(1, len(s)):
+        if s[i - 1] > s[i]:
+            for j in range(i):
+                if s[j] > s[i]:
+                    print(j)
+                    s[j],s[i] = s[i],s[j]
+                    s = sorted(s[:i])[::-1] + s[i:]
+                    return "".join(s[::-1])
     
-    temp_list = []
-    for i in range(1, len(w)):
-        if w[0] < w[i]:
-            temp_list.append(w[i])
+    return "no answer"
 
-    max_idx = w.index(min(temp_list))
-
-    temp_w = sorted(list(w[:max_idx] + w[max_idx + 1:]))
-    return "".join([w[max_idx]] + temp_w)
 
 # 97 ~ 122
 
-print(biggerIsGreater("ab"))  # ba
-print(biggerIsGreater("bb"))  # no answer
-print(biggerIsGreater("hefg"))  # hegf
-print(biggerIsGreater("dhck"))  # dhkc
-print(biggerIsGreater("dkhc"))  # hcdk
-print(biggerIsGreater("abcd"))  # abdc
-print(biggerIsGreater("a"))
+# print(biggerIsGreater("ab"))  # ba
+# print(biggerIsGreater("bb"))  # no answer
 
-print(biggerIsGreater('abdc'))  # acbd
+# print(biggerIsGreater("hefg"))  # hegf
+# print(biggerIsGreater("dhck"))  # dhkc
+
+print(biggerIsGreater("dkhc"))  # hcdk
+# print(biggerIsGreater("abcd"))  # abdc
+# print(biggerIsGreater("a"))
+
+# print(biggerIsGreater('abdc'))  # acbd
