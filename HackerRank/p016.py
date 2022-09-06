1be95582c119d4d5eccaed9dbee3c3b58be16e7a
Kim Min Sung <alstjddl8@naver.com>
2022-09-06 22:08:03 +0900

    Add files via upload

--- HackerRank/p016.py
+++ HackerRank/p016.py
@@ -1,18 +1,27 @@
 # https://www.hackerrank.com/challenges/3d-surface-area/problem?isFullScreen=true
 
 def surfaceArea(A):
-    # Write your code here
+
     answer = 0
-    for a in A:
-        # 첫번째
-        answer += sum(a) + (len(a) * 2) + (max(a) * 2)
+    for i in range(len(A)):
+        if i == 0:
+            answer += sum(A[i]) + (len(A[i]) * 2) + (max(A[i]) * 2)
+            if len(A) == 1: answer += sum(A[0])
 
-        # 중간
+        elif i == len(A) - 1:
+            answer += (len(A[i]) * 2) + (max(A[i]) * 2)
+            for j in range(len(A[i])):
+                answer += abs(A[i][j] - A[i - 1][j])
+            answer += sum(A[i])
 
-        # 마지막
+        else:
+            answer += (len(A[i]) * 2) + (max(A[i]) * 2)
+            for j in range(len(A[i])):
+                answer += abs(A[i][j] - A[i - 1][j])
 
     return answer
 
+print(surfaceArea([[1]]))   # 6
 print(surfaceArea([[1, 3, 4], [2, 2, 3], [1, 2, 4]]))   # 60
 
-#\
+# 2 3   
