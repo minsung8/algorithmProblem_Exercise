6a78126ce172e75d205998577b9d855c5fa11d8e
Kim Min Sung <alstjddl8@naver.com>
2022-09-26 23:59:49 +0900

    Add files via upload

--- HackerRank/p023.py
+++ HackerRank/p023.py
@@ -1,26 +1,56 @@
 # https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem?isFullScreen=true
 
+
 def activityNotifications(expenditure, d):
     # Write your code here
+
+    def get_median(temp_list, d):
+        temp_cnt = 0
+        
+        if d % 2 == 0:
+            mid1, mid2 = 0, 0
+            for j in range(len(temp_list)):
+
+                if temp_list[j] > 0:
+                    temp_cnt += temp_list[j]
+                
+                if mid1 == 0 and temp_cnt >= d // 2:
+                    mid1 = j
+                if mid2 == 0 and temp_cnt >= d // 2 + 1:
+                    mid2 = j
+                    return (mid1 + mid2) / 2
+
+        else:
+            for j in range(len(temp_list)):
+                if temp_list[j] > 0:
+                    temp_cnt += temp_list[j]
+                
+                if temp_cnt >= d // 2 + 1:
+                    return j
+
     answer = 0
+    temp_list = [0] * 200
+
+    for i in range(d):
+        temp_list[expenditure[i]] += 1
 
     for i in range(d, len(expenditure)):
-        temp = sorted(expenditure[i - d:i])
-        mid = _return_mid(temp)
+        if i < d:
+            temp_list[expenditure[i]] += 1
+            continue
 
-        if mid * 2 <= expenditure[i]:
+        median = get_median(temp_list, d)
+
+        if expenditure[i] >= 2 * median:
             answer += 1
+
+        temp_list[expenditure[i - d]] -= 1
+        temp_list[expenditure[i]] += 1
 
     return answer
 
-def _return_mid(_list):
-    _len = len(_list)
-    if _len % 2 != 0: return _list[_len // 2]
-    return (_list[_len // 2 - 1] + _list[_len // 2]) / 2
-
 print(activityNotifications([1, 2, 3, 4, 4], 4))    # 0
-
-
 print(activityNotifications([2, 3, 4, 2, 3, 6, 8, 4, 5], 5))    # 2
 print(activityNotifications([10, 20, 30, 40, 50], 3))   # 1
 
+# 2 3 3 4 6 8 
