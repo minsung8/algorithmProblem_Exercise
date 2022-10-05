543774782368e57a95c0c35d2469b3c838a80f14
Kim Min Sung <alstjddl8@naver.com>
2022-10-05 22:14:19 +0900

    Add files via upload

---
+++ HackerRank/p026.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/richie-rich/problem?isFullScreen=true
+
+def highestValuePalindrome(s, n, k):
+    # Write your code here
+    print( s, n ,k)
+
+
+print(highestValuePalindrome('3943', 4, 1))   # 3993
+print(highestValuePalindrome('092282', 6, 3))   # 992299    
+print(highestValuePalindrome('0011', 4, 1))   # -1
