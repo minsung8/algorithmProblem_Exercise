cc1bb524c58bbc20b4a4bbb9cdc93e67f44590f0
Kim Min Sung <alstjddl8@naver.com>
2022-10-06 23:32:24 +0900

    Add files via upload

--- HackerRank/p026.py
+++ HackerRank/p026.py
@@ -2,7 +2,46 @@
 
 def highestValuePalindrome(s, n, k):
     # Write your code here
-    print( s, n ,k)
+
+    dif_idx_list = []
+
+    front = list(s[:n // 2])
+    back = list(s[n // 2:][::-1])
+
+    for i in range(len(front)):
+        if front[i] != back[i]:
+            dif_idx_list.append(i)
+
+    if len(dif_idx_list) > k: return '-1'
+    elif len(dif_idx_list) == k:
+        
+        for i in range(len(dif_idx_list)):
+            _max = max(int(front[dif_idx_list[i]]), int(back[dif_idx_list[i]]))
+
+            front[dif_idx_list[i]] = str(_max)
+            back[dif_idx_list[i]] = str(_max)
+
+        return "".join(front) + "".join(back[::-1])
+    else:
+        
+        for i in range(len(dif_idx_list)):
+            _max = max(int(front[dif_idx_list[i]]), int(back[dif_idx_list[i]]))
+
+            front[dif_idx_list[i]] = str(_max)
+            back[dif_idx_list[i]] = str(_max)
+        
+            k -= 1
+
+        i = 0
+        while k > 0:
+
+            front[i] = '9'
+            back[i] = '9'
+
+            k -= 1
+            i += 1
+
+    return "".join(front) + "".join(back[::-1])
 
 
 print(highestValuePalindrome('3943', 4, 1))   # 3993
