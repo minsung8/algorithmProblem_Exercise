e6b030d6f233038fc41173c65dbded577460bab0
Kim Min Sung <alstjddl8@naver.com>
2022-10-11 23:32:36 +0900

    Add files via upload

---
+++ HackerRank/p027.py
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem?isFullScreen=true
+
+from collections import defaultdict
+
+def sherlockAndAnagrams(s):
+    # Write your code here
+    dic = defaultdict(list)
+    s = list(s)
+
+    for i in range(len(s)):
+        dic[1].append(s[i])
+    return dic
+    while True:
+
+
+
+
+        break
+        
+
+    return 
+
+
+print(sherlockAndAnagrams('abba'))  # 4
+print(sherlockAndAnagrams('abcd'))  # 0
+
+
+# a b b a
+# ab ab aa 
