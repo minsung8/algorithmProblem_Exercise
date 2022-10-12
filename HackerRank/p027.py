c76bc375d6326dfc465934667932c93ca2437839
Kim Min Sung <alstjddl8@naver.com>
2022-10-12 23:15:32 +0900

    Add files via upload

--- HackerRank/p027.py
+++ HackerRank/p027.py
@@ -1,29 +1,25 @@
 # https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem?isFullScreen=true
 
-from collections import defaultdict
-
+from itertools import combinations
+        
+        
 def sherlockAndAnagrams(s):
     # Write your code here
-    dic = defaultdict(list)
+    cnt = 0
     s = list(s)
 
-    for i in range(len(s)):
-        dic[1].append(s[i])
-    return dic
-    while True:
+    for i in range(len(s) - 1):
+        for j in range(len(s) - i):
+            temp = s[j:j + i + 1]
 
-
-
-
-        break
-        
-
-    return 
-
+            for k in range(j + 1, len(s)):
+                if sorted(temp) == sorted(s[k:k + i + 1]):
+                    cnt += 1
+    return cnt
 
 print(sherlockAndAnagrams('abba'))  # 4
 print(sherlockAndAnagrams('abcd'))  # 0
 
 
 # a b b a
-# ab ab aa 
+# ab ab aa bb ba ba
