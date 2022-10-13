b0d227ece74248cf698be885510606de2abaed39
Kim Min Sung <alstjddl8@naver.com>
2022-10-13 22:58:16 +0900

    Add files via upload

--- HackerRank/p027.py
+++ HackerRank/p027.py
@@ -1,6 +1,4 @@
 # https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem?isFullScreen=true
-
-from itertools import combinations
         
         
 def sherlockAndAnagrams(s):
@@ -8,12 +6,21 @@ def sherlockAndAnagrams(s):
     cnt = 0
     s = list(s)
 
+    dic = {}
+
     for i in range(len(s) - 1):
         for j in range(len(s) - i):
             temp = s[j:j + i + 1]
+            temp_key = "".join(temp)
+            if not dic.get(temp_key):
+                dic[temp_key] = "".join(sorted(temp))
+    
+    for i in range(len(s) - 1):
+        for j in range(len(s) - i):
+            temp_key = "".join(s[j:j + i + 1])
 
             for k in range(j + 1, len(s)):
-                if sorted(temp) == sorted(s[k:k + i + 1]):
+                if dic[temp_key] == dic["".join(s[k:k + i + 1])]:
                     cnt += 1
     return cnt
 
@@ -21,5 +28,3 @@ print(sherlockAndAnagrams('abba'))  # 4
 print(sherlockAndAnagrams('abcd'))  # 0
 
 
-# a b b a
-# ab ab aa bb ba ba
