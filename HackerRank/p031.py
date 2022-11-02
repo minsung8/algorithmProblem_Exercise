b8b9598d33fe2773fb7962c2e3f3fef784a5b7c1
Kim Min Sung <alstjddl8@naver.com>
2022-11-02 23:24:44 +0900

    Add files via upload

--- HackerRank/p031.py
+++ HackerRank/p031.py
@@ -2,7 +2,6 @@
 
 
 def solution(n, info):
-    answer = []
 
     score_list = []
     j = 0
@@ -13,13 +12,33 @@ def solution(n, info):
             score_list.append([0, info[j] + 1, j])
         j += 1
     
+    # 우선순위, 횟수, 인덱스 
     score_list = sorted(score_list, key=lambda x : (x[0], -x[1]), reverse=True)
+    answer = [0] * len(info)
 
-    return score_list
+    for i in range(len(score_list)):
+
+        if score_list[i][1] <= n:
+            n -= score_list[i][1]
+            answer[score_list[i][2]] = score_list[i][1]
+
+        if n <= 0: break
+    
+    a, b = 0, 0
+
+    for i in range(len(info)):
+        if info[i] != 0 or answer[i] != 0:
+            if info[i] >= answer[i]:
+                a += 10 - i
+            else:
+                b += 10 - i
+        print(a, b)
+
+    if a >= b: return [-1]
+
+    return answer
 
 
 
 print(solution(5, [2,1,1,1,0,0,0,0,0,0,0])) # 	[0,2,2,0,1,0,0,0,0,0,0]
-
-
-# 
+print(solution(	1, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
